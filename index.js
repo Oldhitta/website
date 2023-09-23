6c67c43715a3549b48c9208646005ec5a89d61f1
Zaid El Jattari <zaid.26102390@gmail.com>
2023-09-23 15:40:59 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstName = "Zaid";
+
+let age = 29;
+
+let student = true;
+
+console.log("hello", firstName);
+console.log("you are", age);
+console.log("enrolled:", student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
